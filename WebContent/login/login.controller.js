5aef06c17348a647193d150e68c988d8960bfdf3
Aswathigupta <aswathi.g25@gmail.com>
2016-11-09 22:36:45 +0530

    First commit

---
+++ WebContent/login/login.controller.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('LoginController', LoginController);
+
+    LoginController.$inject = ['UserService', '$location', '$rootScope', 'FlashService'];
+    function LoginController(UserService, $location, $rootScope, FlashService) {
+        var vm = this;
+
+        vm.login = login;
+
+        function login() {
+            vm.dataLoading = true;
+            UserService.GetAll()
+                .then(function (response) {
+                    if (response.success) {
+                        FlashService.Success('Login successful', true);
+                        $location.path('/');
+                    } else {
+                        FlashService.Error(response.message);
+                        vm.dataLoading = false;
+                    }
+                });
+        }
+    }
+
+})();
